cd1d9edccb639dc0041f1c6860fcb5aedb9ccf0b
AlexisVillarruel <alexisvillarruelj14@gmail.com>
2024-11-17 18:19:26 -0500

    agrego boton historial

--- app/src/main/java/com/example/promcosermobileapp/ui/personal/PersonalFragment.kt
+++ app/src/main/java/com/example/promcosermobileapp/ui/personal/PersonalFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.promcosermobileapp.R
 import com.example.promcosermobileapp.databinding.FragmentPersonalBinding
 
@@ -18,7 +20,11 @@ class PersonalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view : View = inflater.inflate(R.layout.fragment_personal, container, false)
-
+        val btnhist: Button = view.findViewById(R.id.btnhistorial)
+        val navController = findNavController()
+        btnhist.setOnClickListener {
+            navController.navigate(R.id.personalApiFragment)
+        }
 
         return view
     }
