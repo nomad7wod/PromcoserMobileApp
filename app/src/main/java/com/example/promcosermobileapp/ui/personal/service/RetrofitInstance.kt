04ca1360cb8a223fe1711f2b584f7775c889fdb0
Rodrigo Cabello <rodrigoalejandro220803@gmail.com>
2024-11-26 23:59:10 -0500

    añadiendo interfaces

--- app/src/main/java/com/example/promcosermobileapp/ui/personal/service/RetrofitInstance.kt
+++ app/src/main/java/com/example/promcosermobileapp/ui/personal/service/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private val base_url = "https://9b51-38-25-28-51.ngrok-free.app/api/"
+    private val base_url = "https://13b3-2800-200-e731-a8c-3dc9-3acb-c4e8-7a8d.ngrok-free.app/api/"
 
     private val retrofit by lazy {
         Retrofit.Builder()
