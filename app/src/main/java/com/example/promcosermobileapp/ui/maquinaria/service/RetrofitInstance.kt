01509e08d84f702720382c17169e0a6152fd5c2c
Julio Carrizales <juliocarrizales291@gmail.com>
2024-11-29 02:50:19 -0500

    Lista maquinaria creada - Creación de Servicio de API Maquinaria - Mejoramiento de interfaz del módulo de Maquinaria

--- app/src/main/java/com/example/promcosermobileapp/ui/maquinaria/service/RetrofitInstance.kt
+++ app/src/main/java/com/example/promcosermobileapp/ui/maquinaria/service/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private val base_url = " https://76f8-201-230-248-126.ngrok-free.app/api/"
+    private val base_url = "https://76f8-201-230-248-126.ngrok-free.app/api/"
    
 
     private val retrofit by lazy {
