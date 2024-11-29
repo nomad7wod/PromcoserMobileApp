acdc6785f9b8b5bb3e8ab25c64f9129e1ff702cb
DanielRHYLT <20100771@ue.edu.pe>
2024-11-29 02:10:36 -0500

    HistorialClienteFragment conexion con Api avance

---
+++ app/src/main/java/com/example/promcosermobileapp/ui/cliente/adapter/ClienteAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.promcosermobileapp.ui.cliente.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.promcosermobileapp.R
+import com.example.promcosermobileapp.ui.cliente.model.ClienteModel
+
+class ClienteAdapter(
+    private var lstclientes: List<ClienteModel>
+) : RecyclerView.Adapter<ClienteAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvNombreCliente = itemView.findViewById<TextView>(R.id.tvNombres)
+        val tvApellidoCliente = itemView.findViewById<TextView>(R.id.tvApellidos)
+        val tvDniCliente = itemView.findViewById<TextView>(R.id.tvDNI)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_cliente, parent, false))
+    }
+
+    override fun getItemCount(): Int = lstclientes.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val itemCliente = lstclientes[position]
+        holder.tvNombreCliente.text = itemCliente.nombre
+        holder.tvApellidoCliente.text = itemCliente.apellido
+        holder.tvDniCliente.text = itemCliente.dni
+
+    }
+
+    fun updateCliente(newClienteList: List<ClienteModel>) {
+        lstclientes = newClienteList
+        notifyDataSetChanged()
+    }
+
+
+}
